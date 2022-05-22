333315961050a2ec2818f1f1a7100bd30d2f3e3b
KM <sodp2@naver.com>
2022-05-22 21:45:36 +0900

    feat: 힐트 초기 설정

---
+++ app/src/main/java/com/km/convenience_store/App.kt
@@ -0,0 +1,7 @@
+package com.km.convenience_store
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App: Application()
---
+++ app/src/main/java/com/km/convenience_store/presenter/main/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.km.convenience_store.presenter.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.km.convenience_store.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+    }
+}
